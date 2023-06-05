f3f287bb76aff83adc00badb64ec30afad86ba21
AlGuron <blizgur8@gmail.com>
2023-06-05 23:05:49 +0300

    ref

--- src/telran/tasks/LongestSubsequence.java
+++ src/telran/tasks/LongestSubsequence.java
@@ -1,6 +1,5 @@
 package telran.tasks;
 
-
 public class LongestSubsequence {
 	public static int maxSubsequenceLength(int[] arr) {
 
@@ -17,27 +16,30 @@ public class LongestSubsequence {
 //	Think how to write it in one pass through array.
 //
 //	Implement the tests.
-		if (arr.length == 0) {
-			return 0;
-		}
 		int resultsValue = 0;
+
+		if (arr.length == 0) {
+			return resultsValue;
+		}
+
 		int count = 1;
 		int symbol = arr[0];
+
 		for (int i = 0; i < arr.length; i++) {
 
 			if (i != arr.length - 1 && arr[i + 1] == symbol) {
 				symbol = arr[i];
 				count++;
-
-			} else if (i != arr.length - 1) {
-				resultsValue = Math.max(resultsValue, count);
-				symbol = arr[i + 1];
-				count = 1;
 			} else {
 				resultsValue = Math.max(resultsValue, count);
+
+				if (i != arr.length - 1) {
+					symbol = arr[i + 1];
+					count = 1;
+				}
 			}
 		}
-		
+
 		return resultsValue;
 
 	}
